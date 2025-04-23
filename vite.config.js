b26dde021213818e8aee97eef066b04d3bd8a4d2
Sven Elfgren <sven.elfgren@gmail.com>
2025-04-22 22:06:17 -0700

    switch to esbuil

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true, // Helps with debugging
-    minify: 'terser', // Uses terser for better minification
+    minify: 'esbuild', // Use esbuild instead of terser
     rollupOptions: {
       output: {
         manualChunks: {
